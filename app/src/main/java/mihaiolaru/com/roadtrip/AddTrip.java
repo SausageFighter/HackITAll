e0bb568bc32deca8535c9566b72c0599d546f8f1
Mihai Olaru <mihaialexds@gmail.com>
2018-11-17 10:23:24 +0200

    Init Project

---
+++ app/src/main/java/mihaiolaru/com/roadtrip/AddTrip.java
@@ -0,0 +1,13 @@
+package mihaiolaru.com.roadtrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+    }
+}
