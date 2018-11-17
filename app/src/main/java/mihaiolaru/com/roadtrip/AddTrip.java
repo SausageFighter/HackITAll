0f6cb670f9e2ce38c09caa5d33cdcf2ea16d7829
Mihai Olaru <mihaialexds@gmail.com>
2018-11-17 15:56:16 +0200

    legatura cu map si settings

--- app/src/main/java/mihaiolaru/com/roadtrip/AddTrip.java
+++ app/src/main/java/mihaiolaru/com/roadtrip/AddTrip.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AddTrip extends AppCompatActivity {
 
@@ -20,7 +21,17 @@ public class AddTrip extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String start_coords = ((EditText)findViewById(R.id.editText)).getText().toString();
+                String dest_coords = ((EditText)findViewById(R.id.editText3)).getText().toString();
+                String start_date = ((EditText)findViewById(R.id.editText2)).getText().toString();
+                String end_date = ((EditText)findViewById(R.id.editText4)).getText().toString();
+
                 Intent mapScreen = new Intent(AddTrip.this, ShowTrip.class);
+                mapScreen.putExtra("start_coord", start_coords);
+                mapScreen.putExtra("dest_coord", dest_coords);
+                mapScreen.putExtra("start_date", start_date);
+                mapScreen.putExtra("end_date", end_date);
+
                 startActivity(mapScreen);
             }
         });
@@ -28,8 +39,8 @@ public class AddTrip extends AppCompatActivity {
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             //   Intent settingsActivity = new Intent(AddTrip.this, SettingsActivity.class);
-               // startActivity(settingsActivity);
+                Intent settingsActivity = new Intent(AddTrip.this, SettingsActivity.class);
+                startActivity(settingsActivity);
             }
         });
     }
